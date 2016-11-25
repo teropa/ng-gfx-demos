b48f19e803971c75d0f69d70f1fd1b4b8278f2b5
Tero Parviainen <tero@teropa.info>
2016-11-25 12:54:09 +0200

    CSS-animated logo

---
+++ src/app/animated-logo-css/animated-logo-css.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-logo-css',
+  templateUrl: './animated-logo-css.component.html',
+  styleUrls: ['../logo.scss', './animated-logo-css.component.scss']
+})
+export class AnimatedLogoCssComponent {
+
+  constructor() { }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BasicLogoComponent } from './basic-logo/basic-logo.component';
 import { LogoWithBindingsComponent } from './logo-with-bindings/logo-with-bindings.component';
 
 import { LogoWithComponentsModule } from './logo-with-components/logo-with-components.module';
+import { AnimatedLogoCssComponent } from './animated-logo-css/animated-logo-css.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicLogoComponent,
-    LogoWithBindingsComponent
+    LogoWithBindingsComponent,
+    AnimatedLogoCssComponent
   ],
   imports: [
     BrowserModule,
