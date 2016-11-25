7acd94c20cbf34801e0ea6e7ec83c1eb8ec6f4a3
Tero Parviainen <tero@teropa.info>
2016-11-25 12:11:06 +0200

    More interesting event demo: Follow mouse pointer with rotation

--- src/app/logo-with-bindings/logo-with-bindings.component.ts
+++ src/app/logo-with-bindings/logo-with-bindings.component.ts
@@ -6,15 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['../logo.scss', './logo-with-bindings.component.scss']
 })
 export class LogoWithBindingsComponent {
-  private clicks: string[] = [];
+  rotation = 0;
 
   getAPath() {
     return `M125,52.1L66.8,182.6h0h21.7h0l11.7-29.2h49.4l11.7,29.2h0h21.7h0L125,52.1L125,52.1L125,52.1L125,52.1
       L125,52.1z M142,135.4H108l17-40.9L142,135.4z`;
   }
 
-  onClick() {
-    this.clicks.push('click');
+  onMouseMove(evt: MouseEvent) {
+    const x = evt.offsetX;
+    const y = evt.offsetY;
+    this.rotation = Math.atan2(y - 150, x - 150) + Math.PI / 2;
+  }
+
+  getRotation() {
+    return `rotate(${this.rotation}rad)`;
   }
 
 }
