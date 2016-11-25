eba960e6e9baffa27d0c78060c2de9e467655359
Tero Parviainen <tero@teropa.info>
2016-11-25 10:13:39 +0200

    basic SVG demo

---
+++ src/app/basic-logo/basic-logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-logo',
+  templateUrl: './basic-logo.component.html',
+  styleUrls: ['./basic-logo.component.scss']
+})
+export class BasicLogoComponent {
+}
